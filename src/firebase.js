f10fa8d26288dbc6a50047628e8a27b1102b66f8
billchenziang33 <billziang.chen@mail.utoronto.ca>
2025-10-26 01:21:00 +0800

    commit2

---
+++ src/firebase.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+
+
+  apiKey: "AIzaSyCuRG_LQz7TrfYeQZhk3gKIx8iZ2i3VzEU",
+
+
+  authDomain: "first-hackathon-163ce.firebaseapp.com",
+
+
+  projectId: "first-hackathon-163ce",
+
+
+  storageBucket: "first-hackathon-163ce.firebasestorage.app",
+
+
+  messagingSenderId: "587438600846",
+
+
+  appId: "1:587438600846:web:810a0413a162857703cfdf"
+
+
+}
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Export services
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+// change something to test
